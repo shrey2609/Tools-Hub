bbac7e02bd80666b5154328a80df7be49b5961e6
Devansh sharma <devanshpandit2004@gmail.com>
2025-12-08 13:28:36 +0530

    Change toast error message

--- Backend/services/zoho.js
+++ Backend/services/zoho.js
@@ -60,51 +60,58 @@ export const getOauthToken = async (req, res) => {
 
 // fetches the employee reporting manager from zoho
 export const getReportingManagerEmail = async(email) => {
-    const access_token = await getAccessToken();
-    
-    const employeeResponse = await axios.get(
-      `https://people.zoho.in/api/forms/P_EmployeeView/records?searchColumn=EMPLOYEEMAILALIAS&searchValue=${email}`,
-      {
-        headers: {
-                Authorization: `Zoho-oauthtoken ${access_token}`,
-        },
+  try {
+      const access_token = await getAccessToken();
+      
+      const employeeResponse = await axios.get(
+        `https://people.zoho.in/api/forms/P_EmployeeView/records?searchColumn=EMPLOYEEMAILALIAS&searchValue=${email}`,
+        {
+          headers: {
+                  Authorization: `Zoho-oauthtoken ${access_token}`,
+          },
+        }
+      );
+  
+      const employeeData = employeeResponse.data;
+      if (!employeeData || employeeData.length === 0) {
+        throw new Error("employee with this email does not exist")
       }
-    );
-
-    const employeeData = employeeResponse.data;
-    if (!employeeData || employeeData.length === 0) {
-      throw new Error("No employee found with email")
-    }
-
-    const employee = employeeData[0];
-    const reportingManagerDetails = employee["Reporting To"];
-
-    if (!reportingManagerDetails) {
-      throw new Error("No Reporting Manager found.")
-    }
-    
-    const managerId = reportingManagerDetails.split(" ").pop(); 
-
-    const managerResponse = await axios.get(
-      `${ZOHO_BASE_URL}searchColumn=Employeeid&searchValue=${managerId}`,
-      {
-        headers: {
-          Authorization: `Zoho-oauthtoken ${access_token}`,
-        },
+  
+      const employee = employeeData[0];
+      if (!employee["Reporting To"]) {
+        throw new Error("Employee with this email does not exist");
       }
-    );
 
-    const managerData = managerResponse.data;
-    if (!managerData || managerData.length === 0) {
-      throw new Error("No manager found with id")
-    }
-    const manager = managerData[0];
-    const reportingManagerEmail = manager["Email ID"];
+      const reportingManagerDetails = employee["Reporting To"];
+      if (!reportingManagerDetails) {
+        throw new Error("Reporting manager not assigned for this employee")
+      }
+      
+      const managerId = reportingManagerDetails.split(" ").pop(); 
+      const managerResponse = await axios.get(
+        `${ZOHO_BASE_URL}searchColumn=Employeeid&searchValue=${managerId}`,
+        {
+          headers: {
+            Authorization: `Zoho-oauthtoken ${access_token}`,
+          },
+        }
+      );
+  
+      const managerData = managerResponse.data;
+      if (!managerData || managerData.length === 0) {
+        throw new Error("Reporting manager record not found")
+      }
+      const manager = managerData[0];
+      const reportingManagerEmail = manager["Email ID"];
+  
+      if (!reportingManagerEmail) {
+        throw new Error("Reporting Manager email not found")
+      }
+   
+      return reportingManagerEmail;
 
-    if (!reportingManagerEmail) {
-      throw new Error("Manager email not found")
-    }
-     
-    return reportingManagerEmail;
-
+} catch (error) {
+    console.error("Zoho error:",  error.message);
+    throw new Error(error.message);
+  }
 };
